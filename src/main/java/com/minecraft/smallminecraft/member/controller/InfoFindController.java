94b6000bf8449e75a5d2258da164fbea7cab7a0c
yjin-desktop <yjinlee99@gmail.com>
2024-08-08 16:05:11 +0900

    error: 비밀번호 찾기 수정:

--- src/main/java/com/minecraft/smallminecraft/member/controller/InfoFindController.java
+++ src/main/java/com/minecraft/smallminecraft/member/controller/InfoFindController.java
@@ -7,7 +7,11 @@ import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/account")
 public class InfoFindController {
     private final MemberService memberService;
 
---
+++ src/main/java/com/minecraft/smallminecraft/server/controller/ServerController.java
@@ -0,0 +1,11 @@
+package com.minecraft.smallminecraft.server.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/server")
+public class ServerController {
+
+
+}
